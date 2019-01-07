57fb6745d80204470cbf4e71dc66bab8b7b0d305
Noé Falzon <noe@yousician.com>
2019-01-07 16:17:58 +0200

    Polyamond generation

---
+++ test.lua
@@ -0,0 +1,22 @@
+local make_polyamonds = require "namonds"
+
+local function print_shape(s)
+
+	for _,t in ipairs(s) do
+		io.write(table.concat(t, ","))
+		io.write " / "
+	end
+	print ""
+end
+
+local foo = make_polyamonds(6)
+
+for i = 1,6 do
+	print "======"
+	print(i)
+	print ""
+	
+	for _,v in ipairs(foo[i]) do
+		print_shape(v)
+	end
+end
